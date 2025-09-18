fc8c1d5f3e7ddc3fe701d27949a167973f03c6f9
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-09-18 11:14:55 +0900

    座標の型を整数から少数へ変更。 プレイヤーの情報を表示を追加。

--- GitHubHomeWork2.cpp
+++ GitHubHomeWork2.cpp
@@ -3,12 +3,13 @@
 #include	<ctime>
 using namespace std;
 
+
 const int MAX_NAME = 16;
 
 struct DynamicObject {
 	char name[MAX_NAME];
-	int TransformPositionX;
-	int TransformPositionY;
+	double TransformPositionX;
+	double TransformPositionY;
 	int ImageID;
 };
 
@@ -36,13 +37,31 @@ struct Enemy {
 	bool BossFlag;
 };
 
+void drawMainCaracterParam(const MainCharacter& mc);
+
 int main() {
 	Player player[]{
-		{"浅井誠",50,100,20403545,10,50,100,30},
-		{"桜真央",200,100,30102256,3,30,70,22},
-		{"春風祐樹",150,300,60321076,15,70,130,65},
-		{"魔高重孝",500,400,80306045,20,100,150,100}
+		{"浅井誠",50.4,100.3,20403545,10,50,100,30},
+		{"桜真央",200.7,100.9,30102256,3,30,70,22},
+		{"春風祐樹",150.1,300.9,60321076,15,70,130,65},
+		{"魔高重孝",500.0,400.1,80306045,20,100,150,100}
 	};
 
 	Enemy enemy{ "オーガ・シン",600,600,10200045,true,true };
+
+	for (int i = 0; i < sizeof player / sizeof * player; i++) {
+		drawMainCaracterParam(player[i].Character);
+	}
+}
+
+void drawMainCaracterParam(const MainCharacter& mc) {
+	cout << "名前:" << mc.Object.name << endl;
+	cout << "x座標:" << mc.Object.TransformPositionX << endl;
+	cout << "y座標:" << mc.Object.TransformPositionY << endl;
+	cout << "画像ID:" << mc.Object.ImageID << endl;
+	cout << "レベル:" << mc.LevelOfViolence << endl;
+	cout << "体力:" << mc.HP << endl;
+	cout << "魔力:" << mc.MP << endl;
+	cout << "攻撃力:" << mc.Attack << endl;
+	cout << endl;
 }
